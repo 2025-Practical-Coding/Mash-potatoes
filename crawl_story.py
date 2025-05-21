4931f2ce5e8b9d88f8c3addd917343578b730796
juhwanz <ghdwnghks209@naver.com>
2025-05-21 23:49:20 +0900

    공허지역 데이터 추출

--- crawl_story.py
+++ crawl_story.py
@@ -1,15 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
 
-ROOT_URL = "https://universe.leagueoflegends.com/ko_KR/region"
+# 크롤링할 API 주소 (지역) | 일단 공허 지역
+URL = "https://universe-meeps.leagueoflegends.com/v1/ko_kr/factions/void/index.json"
 
-REGIONS_URL = {
-  "공허" : "void",
-  "녹서스" : "noxus",
-  "데마시아" : "demacia",
-  "밴들시티" : "bandle-city",
-  "빌지워터" : "bilgewater",
-  "프렐요드" : "freljord",
-  "필트오버" : "piltover",
-  "아이오니아" : "ionia",
-}
+# API로 데이터 받아오기
+response = requests.get(URL)
+data = response.json()        #받은 응답을 파이썬 딕셔너리로 변환
+
+# 'associated-champions'라는 키에 챔피언 목록이 들어 있음
+champions_data = data.get("associated-champions", [])
+
+# 오류 메시지
+if not champions_data:
+  print("데이터 없음")
+else :
+  print("챔피언 목록")
+  i = 1
+  for champ_data in champions_data:
+    # 이름
+    if "name" in champ_data:
+      name = champ_data["name"]
+    else:
+      name = ""
+    # slug (영문 명)
+    if "slug" in champ_data:
+      slug = champ_data["slug"]
+    else:
+      slug = ""
+    # 별명
+    if "subtitle" in champ_data:
+      subtitle = champ_data["subtitle"]
+    else:
+      subtitle = ""
+    # 스토리 (일단 일부만)
+    # biography 안에 full 안에 스토리가 있음 ( quote 축약판도 있음 )
+    if "biography" in champ_data and "full" in champ_data["biography"]:
+      story = champ_data["biography"]["full"][:300]
+      soup = BeautifulSoup(story, 'html.parser')
+      origin_story = ' '.join(c for c in soup.strings)
+    else:
+      origin_story = ""
+
+    #테스트 출력
+    print(str(i)+ ". "+ name+" [" + slug+"]" )
+    print("    별명:", subtitle)
+    print("   스토리:", origin_story)
+    print("--------------------------------------------")
+    i +=1
